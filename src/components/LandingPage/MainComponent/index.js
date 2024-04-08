2c800d46552fbddf94ed681cbfdd32e915e09cc7
ritikupadhyay <97518015+ritikupadhyay@users.noreply.github.com>
2024-04-08 15:24:04 +0530

    Update index.js

--- src/components/LandingPage/MainComponent/index.js
+++ src/components/LandingPage/MainComponent/index.js
@@ -48,7 +48,7 @@ function MainComponent() {
           <RWebShare
             data={{
               text: "CryptoDashboard made by Ritik Upadhyay using React JS.",
-              url: "https://crypto-dashboard-jan.netlify.app",
+              url: "https://crypto-trackverse.vercel.app/",
               title: "CryptoTracker.",
             }}
             onClick={() => toast.info("App Shared!")}
